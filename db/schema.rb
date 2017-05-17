40b59dfd2224c92e53f37ce9cb80f76dd5fd063d
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-17 11:43:52 +0200

    Add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517091856) do
+ActiveRecord::Schema.define(version: 20170517094241) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name",        null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20170517091856) do
     t.string   "category",    null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "rating"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Il forno di Napoli",
+    phonenumber:  "01.90.87.79.90",
+    category:     "Italian",
+  },
+  {
+    name:         "Le Dome",
+    phonenumber:  "01.56.65.65.46",
+    category:     "French",
+  },
+  {
+    name:         "Chez FOO",
+    phonenumber:  "01.23.87.29.65",
+    category:     "Chinese",
+  },
+  {
+    name:         "La Vieille Porte",
+    phonenumber:  "01.12.57.71.23",
+    category:     "Belgian",
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
